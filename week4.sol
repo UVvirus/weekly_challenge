9bec9b2c369b75891abb2483172e951ea241ffb4
UVvirus <39599249+UVvirus@users.noreply.github.com>
2023-04-20 16:38:19 +0530

    Update week4.sol

--- week4.sol
+++ week4.sol
@@ -3,8 +3,9 @@ pragma solidity 0.8.17;
 import "./week4_token.sol";
 
 interface IToken {
-    function balanceOf(address account) external view returns(uint);
-    function transferFrom(address from, address to, uint amount) external retruns(uint);
+    function balanceOf(address account) external view returns (uint);
+    function transferFrom(address from, address to, uint amount) external returns (uint);
+    function transfer(address to, uint amount) external;
 }
 
 contract Staking {
@@ -13,7 +14,7 @@ contract Staking {
     mapping(address => stakeinfo) TrackStaked;
 
     uint three_month_duration= 1000;
-    uint _expiry;
+    uint _expiry=1000;
     uint interestRate=30;
 
     struct stakeinfo{
@@ -23,8 +24,11 @@ contract Staking {
         uint claimed;
     }
 
-    constructor(address _tokenAddress){
-        IToken token = IToken(_tokenAddress);
+    event RewardsClaimed(address receipient, uint amount);
+    IToken Token;
+
+    constructor(IToken _tokenAddress){
+         Token = IToken(_tokenAddress);
         uint expiry = block.timestamp + _expiry;
     }
 
@@ -32,11 +36,11 @@ contract Staking {
     function stake(uint amount) external {
 
         require(amount > 0, "zero tokens cant be deposited");
-        require(block.timestamp < expiry, "Expired");
+        require(block.timestamp < _expiry, "Expired");
         require(TrackAddress[msg.sender] == false,"Already staked");
-        require(token.balanceOf(msg.sender) >= amount,"Insufficient tokens");
+        require(Token.balanceOf(msg.sender) >= amount,"Insufficient tokens");
 
-        token.transferFrom(msg.sender,address(this),amount);
+        Token.transferFrom(msg.sender,address(this),amount);
 
         TrackStaked[msg.sender]= stakeinfo({
             startTime: block.timestamp,
@@ -49,13 +53,21 @@ contract Staking {
     }
 
     function claimRewards() external {
-        require(block.timestamp  > expiry,"Not expired");
-
+        require(block.timestamp  > _expiry,"Not expired");
+        uint rewardd = calculateReward();
+        TrackStaked[msg.sender].claimed= rewardd;
+        Token.transfer(msg.sender,rewardd);
+        emit RewardsClaimed(msg.sender, rewardd);
     }
 
-    function calculateReward() {
+    function calculateReward() internal returns(uint){
         uint amountStaked = TrackStaked[msg.sender].stakedAmount;
         uint rewardTokens = amountStaked + (amountStaked * interestRate)/ 100;
+        return rewardTokens;
     }
+
+    receive() external payable{}
+
+    fallback() external payable {}
     
 }
